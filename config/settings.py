3499097ae70ed0e9bad9e696cc1d8ec3f800632d
amourtos <amourtos@gmail.com>
2021-09-17 13:30:33 -0400

    changed settings back to development

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-)v3ahiqgb)r727gz1la!#a3w1iawv0e4#g+)lc)m$pp#k#pl$u'
-SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY', 'django-insecure-)v3ahiqgb)r727gz1la!#a3w1iawv0e4#g+)lc)m$pp#k#pl$u')
+SECRET_KEY = 'django-insecure-)v3ahiqgb)r727gz1la!#a3w1iawv0e4#g+)lc)m$pp#k#pl$u'
+# SECRET_KEY = os.environ.get(
+# 'DJANGO_SECRET_KEY', 'django-insecure-)v3ahiqgb)r727gz1la!#a3w1iawv0e4#g+)lc)m$pp#k#pl$u')
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get("DJANGO_DEBUG", '') != "False"
+DEBUG = True
+# DEBUG = os.environ.get("DJANGO_DEBUG", '') != "False"
 
 ALLOWED_HOSTS = []
 
